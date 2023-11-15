bced2222fafea66f09caf4cb9b43c298a3400e98
Alumno <Alumno@2DAM01>
2023-11-15 18:36:24 +0100

    añadido cambio de santa teresa de jesus

--- src/javaTipsMensajes/VentanaBotones.java
+++ src/javaTipsMensajes/VentanaBotones.java
@@ -5,6 +5,10 @@ public class VentanaBotones {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hma mundo");
+		System.out.println("y muero porque no muero");
+		System.out.println("hma mundo");
+		
+		
 
 	}
 
