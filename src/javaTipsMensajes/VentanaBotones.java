87f09b58c82a84695db89b1d314ec2795ee4c255
Alumno <Alumno@2DAM01>
2023-11-15 18:32:50 +0100

    Primera sincronización

---
+++ src/javaTipsMensajes/VentanaBotones.java
@@ -0,0 +1,11 @@
+package javaTipsMensajes;
+
+public class VentanaBotones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hma mundo");
+
+	}
+
+}
---
+++ src/javaTipsMensajes/VentanaForm.java
@@ -0,0 +1,56 @@
+package javaTipsMensajes;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+
+public class VentanaForm {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaForm window = new VentanaForm();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VentanaForm() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton btnNewButton = new JButton("Botón 1");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		frame.getContentPane().add(btnNewButton);
+	}
+
+}
